8312f7ff65516049c19d18400f5a5d5955be0fd2
minjeong.kim512 <minjeong.kim512@gmail.com>
2023-10-31 17:02:00 +0900

    feator: epic 수정 API 개발
    Signed-off-by: minjeong.kim512 <minjeong.kim512@gmail.com>

--- backend/src/main/java/com/projpolice/domain/epic/controller/EpicController.java
+++ backend/src/main/java/com/projpolice/domain/epic/controller/EpicController.java
@@ -3,6 +3,7 @@ package com.projpolice.domain.epic.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projpolice.domain.epic.dto.EpicDetailData;
 import com.projpolice.domain.epic.request.EpicCreateRequest;
+import com.projpolice.domain.epic.request.EpicUpdateRequest;
 import com.projpolice.domain.epic.service.EpicService;
 import com.projpolice.global.common.base.BaseResponse;
 
@@ -53,6 +55,25 @@ public class EpicController {
                 .code(HttpStatus.OK.value())
                 .message("할일 상세 조회 성공")
                 .data(epicService.getEpic(epiceId))
+                .build());
+    }
+
+    /**
+     * 해당 할일을 수정하는 요청 처리
+     *
+     * @param epiceId
+     * @param epicUpdateRequest
+     * @return 수정된 할일 상세 정보 조회
+     */
+    @PatchMapping("/{epiceId}")
+    public ResponseEntity<BaseResponse<EpicDetailData>> updateEpic(@PathVariable("epiceId") Long epiceId
+        , @RequestBody EpicUpdateRequest epicUpdateRequest) {
+
+        return ResponseEntity.ok()
+            .body(BaseResponse.<EpicDetailData>builder()
+                .code(HttpStatus.OK.value())
+                .message("할일 수정 성공")
+                .data(epicService.updateEpic(epiceId, epicUpdateRequest))
                 .build());
     }
 }
