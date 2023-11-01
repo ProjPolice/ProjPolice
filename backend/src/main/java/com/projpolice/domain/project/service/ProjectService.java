0ae385755a13865ae8e2bda950121b98cb2732a1
정우정 <wj6349@gmail.com>
2023-11-01 14:20:34 +0900

    feat: 프로젝트 팀원 조회 기능 추가
    closes S09P31A506-275
    
    Signed-off-by: 정우정 <wj6349@gmail.com>

--- backend/src/main/java/com/projpolice/domain/project/service/ProjectService.java
+++ backend/src/main/java/com/projpolice/domain/project/service/ProjectService.java
@@ -1,8 +1,11 @@
 package com.projpolice.domain.project.service;
 
+import java.util.List;
+
 import com.projpolice.domain.project.dto.ProjectDetailData;
 import com.projpolice.domain.project.request.ProjectInsertRequest;
 import com.projpolice.domain.project.request.ProjectModifyRequest;
+import com.projpolice.domain.user.dto.UserIdNameImgItem;
 import com.projpolice.global.common.base.BaseIdItem;
 
 public interface ProjectService {
@@ -13,4 +16,6 @@ public interface ProjectService {
     BaseIdItem deleteProject(long id);
 
     ProjectDetailData modifyProject(long id, ProjectModifyRequest request);
+
+    List<UserIdNameImgItem> listProjectUser(long id);
 }
