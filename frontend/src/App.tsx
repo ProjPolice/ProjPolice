3901024ace70214b654093ae1f0870208288341a
ikthegreat <ikthegreat@icloud.com>
2023-11-02 16:36:18 +0900

    Feat: Global Style 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import { globalStyle } from '@assets/design/globalStyles';
+import { Global } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-  <div>
-    <Outlet />
-  </div>
-  )
+    <div>
+      <Global styles={globalStyle} />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
