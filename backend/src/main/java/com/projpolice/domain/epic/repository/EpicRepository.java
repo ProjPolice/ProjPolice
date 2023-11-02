3853765e754ab12d6af45cc225447c53d62e16bb
정우정 <wj6349@gmail.com>
2023-11-02 16:09:56 +0900

    feat: 권한 확인 쿼리 및 ProjectAuthManager method 추가
    closes S09P31A506-269
    
    Signed-off-by: 정우정 <wj6349@gmail.com>

--- backend/src/main/java/com/projpolice/domain/epic/repository/EpicRepository.java
+++ backend/src/main/java/com/projpolice/domain/epic/repository/EpicRepository.java
@@ -1,10 +1,18 @@
 package com.projpolice.domain.epic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.projpolice.domain.epic.domain.Epic;
 
 @Repository
 public interface EpicRepository extends JpaRepository<Epic, Long> {
+    @Query("""
+        select count(epic.id)
+        from Epic epic left join Project project
+        where epic.id = :epic_id and epic.deleted = false and project.user.id = :user_id
+        """)
+    boolean checkMembership(@Param("epic_id") long epicId, @Param("user_id") long userId);
 }
--- backend/src/main/java/com/projpolice/global/common/manager/ProjectAuthManager.java
+++ backend/src/main/java/com/projpolice/global/common/manager/ProjectAuthManager.java
@@ -4,9 +4,11 @@ import static com.projpolice.domain.user.service.JwtService.*;
 
 import org.springframework.stereotype.Component;
 
+import com.projpolice.domain.epic.repository.EpicRepository;
 import com.projpolice.domain.project.domain.Project;
 import com.projpolice.domain.project.repository.ProjectRepository;
 import com.projpolice.domain.project.repository.UserProjectRepository;
+import com.projpolice.domain.task.repository.TaskRepository;
 import com.projpolice.domain.user.domain.User;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,10 @@ public class ProjectAuthManager {
     private final ProjectRepository projectRepository;
 
     private final UserProjectRepository userProjectRepository;
+
+    private final EpicRepository epicRepository;
+
+    private final TaskRepository taskRepository;
 
     /**
      * Checks if the logged user is the owner of the specified project.
@@ -50,5 +56,38 @@ public class ProjectAuthManager {
     public boolean checkProjectMembership(long projectId) {
         User loggedUser = getLoggedUser();
         return userProjectRepository.checkMembership(projectId, loggedUser.getId());
+    }
+
+    /**
+     * Checks if the logged user is a member of the specified epic.
+     *
+     * @param epicId The ID of the epic to be checked.
+     * @return true if the logged user is a member of the epic, false otherwise.
+     */
+    public boolean checkEpicMembership(long epicId) {
+        User loggedUser = getLoggedUser();
+        return epicRepository.checkMembership(epicId, loggedUser.getId());
+    }
+
+    /**
+     * Checks if the logged user is the owner of the specified task.
+     *
+     * @param taskId The ID of the task to be checked.
+     * @return True if the logged user is the owner of the task, false otherwise.
+     */
+    public boolean checkTaskOwnership(long taskId) {
+        User loggedUser = getLoggedUser();
+        return taskRepository.checkOwnership(taskId, loggedUser.getId());
+    }
+
+    /**
+     * Checks if the logged user is a member of the specified task.
+     *
+     * @param taskId The ID of the task to be checked.
+     * @return True if the logged user is a member of the task, false otherwise.
+     */
+    public boolean checkTaskMembership(long taskId) {
+        User loggedUser = getLoggedUser();
+        return taskRepository.checkMembership(taskId, loggedUser.getId());
     }
 }
