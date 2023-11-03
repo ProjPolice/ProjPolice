49f15f96aee96e21578ae5263ce923d910e6d361
ChangJae-YOO <dondegi5@gmail.com>
2023-11-03 17:26:00 +0900

    feat: 파일 조회, 업로드, 삭제 구현

---
+++ backend/src/main/java/com/projpolice/domain/file/dto/FileDetailItem.java
@@ -0,0 +1,30 @@
+package com.projpolice.domain.file.dto;
+
+import com.projpolice.domain.file.domain.File;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FileDetailItem {
+    private Long id;
+    private String name;
+    private String comment;
+    private String uuid;
+    private Integer version;
+    private String extension;
+    private Long taskId;
+
+    public static FileDetailItem from(File file) {
+        return FileDetailItem.builder()
+            .id(file.getId())
+            .name(file.getName())
+            .comment(file.getComment())
+            .uuid(file.getUuid())
+            .version(file.getVersion())
+            .extension(file.getExtension())
+            .taskId(file.getTask().getId())
+            .build();
+    }
+}
