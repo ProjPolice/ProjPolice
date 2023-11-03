91e1d2f02bafe9d8e75b53ae346b97c8477c1172
minjeong.kim512 <minjeong.kim512@gmail.com>
2023-11-03 16:14:00 +0900

    feat: 오라클 클라우드 스토리지 기능 추가
    Signed-off-by: minjeong.kim512 <minjeong.kim512@gmail.com>

--- backend/src/main/java/com/projpolice/BackendApplication.java
+++ backend/src/main/java/com/projpolice/BackendApplication.java
@@ -2,8 +2,12 @@ package com.projpolice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.projpolice.global.storage.oracle.OracleCloudObjectStorageConstantProvider;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OracleCloudObjectStorageConstantProvider.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/projpolice/global/common/error/exception/FileException.java
@@ -0,0 +1,14 @@
+package com.projpolice.global.common.error.exception;
+
+import com.projpolice.global.common.error.info.ExceptionInfo;
+
+/**
+ * The NotFoundException class is a custom exception class that extends the BaseException class.
+ * It is used to indicate that a resource or object is not found in the system.
+ */
+public class FileException extends BaseException {
+
+    public FileException(ExceptionInfo exceptionInfo) {
+        super(exceptionInfo);
+    }
+}
