89ec8b48a88e9b752f736744147242a178328c57
정우정 <wj6349@gmail.com>
2023-11-07 13:34:55 +0900

    fix: add constructor
    Signed-off-by: 정우정 <wj6349@gmail.com>

--- backend/src/main/java/com/projpolice/domain/user/dto/UserIdNameImgItem.java
+++ backend/src/main/java/com/projpolice/domain/user/dto/UserIdNameImgItem.java
@@ -2,11 +2,15 @@ package com.projpolice.domain.user.dto;
 
 import com.projpolice.domain.user.domain.User;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserIdNameImgItem extends UserIdNameItem {
     private String image;
 
