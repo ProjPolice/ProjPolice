978701b9bd88cfa35bc425d4bf7196d5ac1a29ce
정우정 <wj6349@gmail.com>
2023-11-13 21:50:05 +0900

    feat: Mail

--- backend/src/main/java/com/projpolice/global/notification/mail/service/MailServiceImpl.java
+++ backend/src/main/java/com/projpolice/global/notification/mail/service/MailServiceImpl.java
@@ -4,15 +4,18 @@ import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.projpolice.global.notification.mail.dto.MailDto;
 
+import jakarta.mail.Address;
 import jakarta.mail.Authenticator;
 import jakarta.mail.Message;
 import jakarta.mail.PasswordAuthentication;
 import jakarta.mail.Session;
 import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +40,12 @@ public class MailServiceImpl implements MailService {
 
     private final String domain;
 
+    private final boolean startTLSEnable;
+
+    private final boolean enableSSL;
+
+    private final String nickname;
+
     @Autowired
     MailServiceImpl(Environment env) {
         senderEmail = env.getProperty("projpolice.mail.sender");
@@ -45,6 +54,17 @@ public class MailServiceImpl implements MailService {
         smtpHost = env.getProperty("projpolice.mail.host");
         smtpPort = env.getProperty("projpolice.mail.port");
         domain = env.getProperty("projpolice.mail.domain");
+        nickname = env.getProperty("propolice.mail.nickname");
+        String tmp = env.getProperty("projpolice.mail.properties.mail.smtp.starttls.enable");
+        if (tmp == null) {
+            tmp = "";
+        }
+        startTLSEnable = "true".equalsIgnoreCase(tmp);
+        tmp = env.getProperty("projpolice.mail.properties.mail.smtp.ssl.enable");
+        if (tmp == null) {
+            tmp = "";
+        }
+        enableSSL = "true".equalsIgnoreCase(tmp);
 
         auth = new Authenticator() {
             @Override
@@ -54,22 +74,28 @@ public class MailServiceImpl implements MailService {
         };
     }
 
+    @Async
     @Override
     public void sendMail(MailDto mail) {
         Properties prop = new Properties();
         prop.put("mail.smtp.host", smtpHost);
         prop.put("mail.smtp.port", Integer.parseInt(smtpPort));
-        prop.put("mail.smtp.starttls.enable", false);
+        prop.put("mail.smtp.starttls.enable", startTLSEnable);
+        prop.put("mail.smtp.EnableSSL.enable", enableSSL);
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.quitwait", "false");
+        prop.put("mail.mime.allowutf8", true);
         Session session = Session.getDefaultInstance(prop, auth);
 
         MimeMessage message = new MimeMessage(session);
         try {
-            message.setFrom(domain);
-            message.setFrom(senderEmail);
-            message.addRecipient(Message.RecipientType.TO,
-                new jakarta.mail.internet.InternetAddress(mail.getEmailAddress()));
+            message.setFrom(String.format("%s <%s>", nickname, senderEmail));
+
+            Address[] recipientAddress = InternetAddress.parse(mail.getEmailAddress());
+
+            for (Address address : recipientAddress) {
+                message.addRecipient(Message.RecipientType.TO, address);
+            }
             message.setSubject(mail.getTitle(), encoding);
             message.setText(mail.getBody(), encoding);
             Transport.send(message);
